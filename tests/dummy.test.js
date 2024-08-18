5b0f5d3256d1760ec83a242abc339d200571fa5a
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-08-18 13:20:56 +0300

    Complete exercise 4.3

---
+++ tests/dummy.test.js
@@ -0,0 +1,13 @@
+const { test, describe } = require("node:test");
+
+const listHelper = require("../utils/list_helper");
+
+const assert = require("node:assert");
+
+test("dummy that returns 1", () => {
+  const blogs = [];
+
+  const result = listHelper.dummy(blogs);
+
+  assert.strictEqual(result, 1);
+});
