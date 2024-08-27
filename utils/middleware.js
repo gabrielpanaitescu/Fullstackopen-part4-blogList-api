4ee94f24f2eee5d6b0b601a4f390d07440eedba9
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-08-28 00:26:49 +0300

    Complete exercise 4.22

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require("./logger");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const requestLogger = (request, response, next) => {
   logger.info("Method: ", request.method);
@@ -34,7 +36,7 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
-const extractToken = (request, response, next) => {
+const tokenExtractor = (request, response, next) => {
   const authorization = request.headers.authorization;
   if (authorization && authorization.startsWith("Bearer")) {
     request.token = authorization.replace("Bearer ", "");
@@ -42,9 +44,21 @@ const extractToken = (request, response, next) => {
   next();
 };
 
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!decodedToken.id)
+    return response.status(401).json({ error: "token invalid" });
+
+  request.user = await User.findById(decodedToken.id);
+
+  next();
+};
+
 module.exports = {
   unknownEndpoint,
   requestLogger,
   errorHandler,
-  extractToken,
+  tokenExtractor,
+  userExtractor,
 };
